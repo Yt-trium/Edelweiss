8044e5df120ce92b47c23063e662cc845ed61d63
Yt-trium <cyril.meyer.68@gmail.com>
2017-10-31 01:31:31 +0100

    Array : operator[] + operator==

--- ProjCPOA_Init/vectorMatrix/array.h
+++ ProjCPOA_Init/vectorMatrix/array.h
@@ -2,25 +2,57 @@
 #define ARRAY_H
 
 #include <cstddef>
+#include <stdexcept>
 
 template<class T, std::size_t N>
 class Array
 {
 public:
     Array();
+    Array(T a[N]);
+
+    // operator
+    T const &operator[](std::size_t n) const
+    {
+        std::cout << "const T &operator[](std::size_t n) const" << std::endl;
+        if (n >= N)
+            throw std::out_of_range("Array - operator[]");;
+        return array[n];
+    }
+    T &operator[](std::size_t n)
+    {
+        std::cout << "T &operator[](std::size_t n)" << std::endl;
+        if (n >= N)
+            throw std::out_of_range("Array - operator[]");;
+        return array[n];
+    }
+    bool operator==(const Array<T, N> &a1)
+    {
+        for (int i = 0; i < N; i++)
+            if (a1[i] != array[i])
+                return false;
+        return true;
+    }
 
 private:
     T array[N];
     std::size_t length;
 };
 
+template<class T, std::size_t N>
+Array<T, N>::Array()
+    : length(N)
+{
+    for (int i = 0; i < N; i++)
+        array[i] = 0;
+}
 
 template<class T, std::size_t N>
-Array<T,N>::Array()
-: length(N)
+Array<T, N>::Array(T a[N])
+    : length(N)
 {
-    for(int i=0;i<N;i++)
-        array[i]=0;
+    for (int i = 0; i < N; i++)
+        array[i] = a[i];
 }
 
 
--- ProjCPOA_Init/vectorMatrix/tests/test_array.cpp
+++ ProjCPOA_Init/vectorMatrix/tests/test_array.cpp
@@ -7,9 +7,30 @@ int main()
 {
     std::cout << "<TEST ARRAY> BEGIN" << std::endl;
 
-    assert(1+1==2);
+    assert(1 + 1 == 2);
 
-    Array<int,10> a1();
+    Array<int, 10> a1;
+
+    int toto[3] = {1, 2, 8};
+    Array<int, 3> a2(toto);
+    std::cout << a2[0] << " - " << a2[1] << " - " << a2[2] << std::endl;
+    // std::cout << a2[3] << std::endl; // std::out_of_range
+    a2[0]++;
+    std::cout << a2[0] << " - " << a2[1] << " - " << a2[2] << std::endl;
+
+    Array<int, 3> a3;
+    a3[0] = 1;
+    a3[1] = 2;
+    a3[2] = 8;
+
+    std::cout << "a2 == a3 : " << (a2 == a3) << std::endl;
+    if (a2 == a3)
+        return false;
+
+    a3[0]++;
+    std::cout << "a2 == a3 : " << (a2 == a3) << std::endl;
+    if (!(a2 == a3))
+        return false;
 
     std::cout << "<TEST ARRAY> END" << std::endl;
 
